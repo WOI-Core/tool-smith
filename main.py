e8a2ca906c672e5470e16fd9d1fc1026312820f0
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-06-21 22:33:28 +0700

    init: main.py
    also fixed some prompt

---
+++ main.py
@@ -0,0 +1,55 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from tempfile import SpooledTemporaryFile
+from fastapi import UploadFile
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+# init
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# RAG for example
+# loader = TextLoader("task_gen/example.txt", encoding="utf-8")
+# example = loader.load()
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=60)
+# chunks = text_splitter.split_documents(example)
+# embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+# vectorstore = FAISS.from_documents(chunks, embedding)
+# retrievers = vectorstore.as_retriever()
+
+# Load prompt
+with open("prompt.txt", "r", encoding="utf-8") as f:
+    prompt = f.read()
+
+# Call from API
+class contentFromUser(BaseModel):
+    content_name: str
+
+async def generate_task(contentFromUser: contentFromUser) -> list[UploadFile]:
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "คุณคือคนที่ต้องสร้างโจทย์ competetive programming โดยต้องทำตามโครงสร้างใน human message อย่างเคร่งครัด"),
+        ("human", prompt)#+"รูปแบบต้องเหมือนกับเอกสารนี้ {context}")
+    ])
+    chain = prompt | llm
+    content_name = contentFromUser.content_name.lower().replace(" ", "_")
+    res = await chain.ainvoke({"content": content_name})#, "context": retrievers
+
+    taskfile = res.content.split("________________________________________")
+    task_name = taskfile[0].replace("\n", "")
+
+    task_files = []
+    file_name = ["generate_input.py", "README.md", f"{task_name}.cpp"]
+
+    for file, name in zip(taskfile, file_name):
+        temp_file = SpooledTemporaryFile()
+        temp_file.write(file.encode("utf-8"))
+        temp_file.seek(0)
+        task_files.append(UploadFile(filename=name, file=temp_file))
+
+    return task_files
