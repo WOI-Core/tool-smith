1e781f250d4ec16c73e95124d0063dc239d3e79c
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-06-23 21:56:18 +0700

    feat: testcases

--- main.py
+++ main.py
@@ -37,6 +37,9 @@ async def generate_task(requestFromUser: requestFromUser) -> list[UploadFile]:
 
     for i in [0, 2, 3]: task_string[i] = backtickFilter(task_string[i])
 
+    print(task_string[0])
+    testcases = testcases_generate(task_string[0])
+
     task_files = []
     file_name = ["generate_input.py", "README.md", f"{task_name}.cpp", "config.json"]
 
@@ -46,7 +49,36 @@ async def generate_task(requestFromUser: requestFromUser) -> list[UploadFile]:
         temp_file.seek(0)
         task_files.append(UploadFile(filename=name, file=temp_file))
 
+    input_files = []
+    for i in range(0, len(testcases[0])):
+        temp_file = SpooledTemporaryFile()
+        temp_file.write(testcases[0][i].encode("utf-8"))
+        temp_file.seek(0)
+        input_files.append(UploadFile(filename="input/input"+str(i).zfill(2), file=temp_file))
+    task_files.extend(input_files)
+
+    output_files = []
+    for i in range(0, len(testcases[1])):
+        temp_file = SpooledTemporaryFile()
+        temp_file.write(testcases[1][i].encode("utf-8"))
+        temp_file.seek(0)
+        output_files.append(UploadFile(filename="output/output"+str(i).zfill(2), file=temp_file))
+    task_files.extend(output_files)
+
     return task_files
+
+def testcases_generate(code: str) -> list[list[str], list[str]]:
+    code = "import random\n" + code
+    local_vars = {}
+    exec(code, {}, local_vars)
+
+    if "generate_test_cases" not in local_vars:
+        raise ValueError("No function named generate_test_cases found in generated code.")
+
+    generate_test_cases = local_vars["generate_test_cases"]
+    test_input, test_output = generate_test_cases()
+
+    return [test_input, test_output]
 
 def backtickFilter(text):
     filtered_lines = [line for line in text.splitlines() if "```" not in line]
